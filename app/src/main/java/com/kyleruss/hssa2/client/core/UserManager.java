3d562d82b632fdc43e9ecf0d470325d9fa1626ce
kyle <kyle.business@inbox.com>
2016-10-12 23:39:34 +1300

    Added UserManager

---
+++ app/src/main/java/com/kyleruss/hssa2/client/core/UserManager.java
@@ -0,0 +1,12 @@
+//======================================
+//  Kyle Russell
+//  AUT University 2016
+//  Highly Secured Systems A2
+//======================================
+
+package com.kyleruss.hssa2.client.core;
+
+public class UserManager
+{
+
+}
