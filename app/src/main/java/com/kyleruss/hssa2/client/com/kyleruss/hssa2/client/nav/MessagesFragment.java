30c54251d362bff8eaebab2f4dbf80058c77bad3
kyle <kyle.business@inbox.com>
2016-10-15 14:08:38 +1300

    Added nav.MessagesFragment

--- app/src/main/java/com/kyleruss/hssa2/client/com/kyleruss/hssa2/client/nav/MessagesFragment.java
+++ app/src/main/java/com/kyleruss/hssa2/client/com/kyleruss/hssa2/client/nav/MessagesFragment.java
@@ -6,8 +6,6 @@
 
 package com.kyleruss.hssa2.client.com.kyleruss.hssa2.client.nav;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,13 +15,15 @@ import android.view.ViewGroup;
 import com.kyleruss.hssa2.client.R;
 
 
-public class SendSMSFragments extends Fragment
+public class MessagesFragment extends Fragment
 {
+    public MessagesFragment() {}
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        getActivity().getActionBar().setTitle("Send SMS");
-        View view   =   inflater.inflate(R.layout.fragment_send_sms, container, false);
+        getActivity().setTitle("Messages");
+        View view   =    inflater.inflate(R.layout.fragment_messages, container, false);
         return view;
     }
 }
