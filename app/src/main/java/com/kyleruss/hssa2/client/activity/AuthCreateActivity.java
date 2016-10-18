67e7e296bdb1cf5013a0db8fcce5b51d25f9217d
kyle <kyle.business@inbox.com>
2016-10-19 00:08:10 +1300

    Added send SMS view

--- app/src/main/java/com/kyleruss/hssa2/client/activity/AuthCreateActivity.java
+++ app/src/main/java/com/kyleruss/hssa2/client/activity/AuthCreateActivity.java
@@ -44,8 +44,6 @@ public class AuthCreateActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_create);
-        //sendPasswordAuthEmail();
-      //  createTempUser();
     }
 
     private void sendPasswordAuthEmail()
@@ -71,63 +69,6 @@ public class AuthCreateActivity extends Activity
         }
     }
 
-    /*private void showConnectActivity()
-    {
-        Intent intent   =   new Intent(this, ConnectActivity.class);
-        startActivity(intent);
-    }
-
-
-    private void sendRegisterPackage()
-    {
-        try
-        {
-            String password = "Y2TLKT7T";
-            //-------------------------------------------------------------------------------
-            //Generate request ID, nonce and salts
-            //-------------------------------------------------------------------------------
-            Map.Entry<String, String> requestEntry = RequestManager.getInstance().generateRequest();
-            String requestID    =   requestEntry.getKey();
-            String nonce        =   requestEntry.getValue();
-            String salt         =   registerUser.getPhoneID().substring(0, 4) + registerUser.getEmail().substring(0, 4);
-
-            //MD5 hash Salt & Pass for required key length
-            byte[] hashedSalt   =   CryptoCommons.generateHash(salt.getBytes("UTF-8"));
-            byte[] hashedPass   =   CryptoCommons.generateHash(password.getBytes("UTF-8"));
-            //-------------------------------------------------------------------------------
-
-            JsonObject userDataObj = CommUtils.prepareAuthenticatedRequest(requestID, nonce);
-            currentKeyPair      =   KeyManager.getInstance().generateClientKeyPair();
-            String publicKey    =   Base64.encodeToString(currentKeyPair.getPublic().getEncoded(), Base64.NO_WRAP);
-            userDataObj.addProperty("publicKey", publicKey);
-            userDataObj.addProperty("phoneID", registerUser.getPhoneID());
-            userDataObj.addProperty("email", registerUser.getEmail());
-            userDataObj.addProperty("name", registerUser.getName());
-            byte[] pbEncryptedData  =   CryptoCommons.pbeEncrypt(hashedPass, hashedSalt, userDataObj.toString().getBytes("UTF-8"));
-            String encodedUserData  =   URLEncoder.encode(Base64.encodeToString(pbEncryptedData, Base64.NO_WRAP), "UTF-8");
-
-            JsonObject authContentsObj = new JsonObject();
-            authContentsObj.addProperty("password", password);
-            authContentsObj.addProperty("salt", salt);
-            byte[] encryptedAuthContents    =   CryptoCommons.publicEncrypt(authContentsObj.toString().getBytes("UTF-8"), KeyManager.getInstance().getServerPublicKey());
-            String encodedAuthContents      =   URLEncoder.encode(Base64.encodeToString(encryptedAuthContents, Base64.NO_WRAP), "UTF-8");
-
-            ServiceRequest request  =   new ServiceRequest(ClientConfig.CONN_URL + RequestPaths.USER_PUBLIC_SEND_REQ, false);
-            request.addParam("authContents", encodedAuthContents);
-            request.addParam("clientData", encodedUserData);
-
-            UserRegisterTask task   =   new UserRegisterTask();
-            task.execute(request);
-            Log.d("SEND_STUFF", "sent stuff");
-        }
-
-        catch(Exception e)
-        {
-            e.printStackTrace();
-            Log.d("SEND_REGISTER_FAIL", "" + e.getMessage());
-        }
-    } */
-
     public void createUser(View v)
     {
         String email    =   ((EditText) findViewById(R.id.regEmailField)).getText().toString();
@@ -147,7 +88,6 @@ public class AuthCreateActivity extends Activity
             registerUser = new User(phoneID, name);
             registerUser.setEmail(email);
             sendPasswordAuthEmail();
-          //  sendRegisterPackage();
         }
     }
 
@@ -158,42 +98,6 @@ public class AuthCreateActivity extends Activity
         startActivity(intent);
     }
 
-
-   /* private class UserRegisterTask extends HTTPAsync
-    {
-        @Override
-        protected void onPostExecute(String response)
-        {
-            try
-            {
-                JsonObject responseObj = CommUtils.parseJsonInput(response);
-                byte[] key = Base64.decode(responseObj.get("key").getAsString(), Base64.DEFAULT);
-                byte[] data = Base64.decode(responseObj.get("data").getAsString(), Base64.DEFAULT);
-                EncryptedSession encSession = new EncryptedSession(key, data, currentKeyPair.getPrivate());
-                encSession.unlock();
-                JsonObject  decryptedResponse   =   CommUtils.parseJsonInput(new String(encSession.getData()));
-
-                if(decryptedResponse.get("status").getAsBoolean())
-                {
-                    String nonce        =   decryptedResponse.get("nonce").getAsString();
-                    String requestID    =   decryptedResponse.get("requestID").getAsString();
-
-                    if(RequestManager.getInstance().verifyAndDestroy(requestID, nonce))
-                    {
-                        KeyManager.getInstance().setClientKeyPair(currentKeyPair);
-                        KeyManager.getInstance().saveClientKeyPair(AuthCreateActivity.this);
-                        showConnectActivity();
-                        Log.d("REGISTER_TASK_COMPLETE", decryptedResponse.get("statusMessage").getAsString());
-                    }
-                }
-            }
-
-            catch (Exception e)
-            {
-                Log.d("REGISTER_TASK_FAIL", e.getMessage());
-            }
-        }
-    } */
 
     private class PasswordSendTask extends HTTPAsync
     {
