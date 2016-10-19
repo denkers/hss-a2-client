e08a67f381ae617c7f77ad8d16e80a5e25267180
kyle <kyle.business@inbox.com>
2016-10-19 15:31:26 +1300

    Added communication.SMSReciever

---
+++ app/src/main/java/com/kyleruss/hssa2/client/communication/SMSReceiver.java
@@ -0,0 +1,20 @@
+//======================================
+//  Kyle Russell
+//  AUT University 2016
+//  Highly Secured Systems A2
+//======================================
+
+package com.kyleruss.hssa2.client.communication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SMSReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        
+    }
+}
